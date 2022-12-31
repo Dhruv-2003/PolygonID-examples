205eb38edd5342506184bebf063bb22d6e94d686
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2022-12-31 17:52:49 +0530

    Implemented and tried on chain polygon Id verification

--- on-chain-verification/scripts/set-request.js
+++ on-chain-verification/scripts/set-request.js
@@ -1,31 +1,29 @@
 async function main() {
-
   const circuitId = "credentialAtomicQuerySig";
   const validatorAddress = "0xb1e86C4c687B85520eF4fd2a0d14e81970a15aFB";
 
-  const schemaHash = "f03ac39aa54a5a2770a30f17d8042507" // extracted from PID Platform
+  const schemaHash = "020b434e4e1ea7175c7617f31fd0e3bb"; // extracted from PID Platform
 
-  const schemaEnd = fromLittleEndian(hexToBytes(schemaHash))
+  const schemaEnd = fromLittleEndian(hexToBytes(schemaHash));
 
   const query = {
     schema: ethers.BigNumber.from(schemaEnd),
     slotIndex: 2,
     operator: 2,
-    value: [20020101, ...new Array(63).fill(0).map(i => 0)],
+    value: [20020101, ...new Array(63).fill(0).map((i) => 0)],
     circuitId,
-    };
+  };
 
   // add the address of the contract just deployed
-  ERC20VerifierAddress = "0x31E9dD4c5DD72ad0eb8260C2b32BEa67Fad63F3b"
+  ERC20VerifierAddress = "0xfBa01D7D8Ebe71d31670c8cfA6Bff6f804d9D8fa";
 
-  let erc20Verifier = await hre.ethers.getContractAt("ERC20Verifier", ERC20VerifierAddress)
+  let erc20Verifier = await hre.ethers.getContractAt(
+    "ERC20Verifier",
+    ERC20VerifierAddress
+  );
 
   try {
-    await erc20Verifier.setZKPRequest(
-      1,
-      validatorAddress,
-      query
-    );
+    await erc20Verifier.setZKPRequest(1, validatorAddress, query);
     console.log("Request set");
   } catch (e) {
     console.log("error: ", e);
@@ -34,7 +32,7 @@ async function main() {
 
 function hexToBytes(hex) {
   for (var bytes = [], c = 0; c < hex.length; c += 2)
-      bytes.push(parseInt(hex.substr(c, 2), 16));
+    bytes.push(parseInt(hex.substr(c, 2), 16));
   return bytes;
 }
 
